656fac09caf0fa0bb30409d1e945b6edc50d319f
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-11-01 06:58:51 +0000

    [Fix]README & routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
