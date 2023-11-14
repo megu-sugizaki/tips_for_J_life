c70e94b07372a3b52e9da9c1cac32ef04e43ffcc
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-11-14 05:12:54 +0000

    [Add]Admin layout completed

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,22 @@
 class Admin::EventsController < ApplicationController
+    
+    def index
+        @events = Event.all
+    end 
+    
+    def show
+        @event = Event.find(params[:id])
+    end 
+    
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+        redirect_to admin_events_path
+    end 
+    
+    private
+    
+    def event_params
+      params.require(:event).permit(:title, :caption)
+    end
 end
