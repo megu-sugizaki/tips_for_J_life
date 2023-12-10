5b62e551aa907ca9990414a98106a47370bb71f3
megu-sugizaki <143850606+megu-sugizaki@users.noreply.github.com>
2023-12-10 15:35:44 +0000

    [Feature]Bookmark asynchronous communication completed

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -5,12 +5,16 @@ class Public::BookmarksController < ApplicationController
     # M:To prevent consecutive hits
     @bookmark = current_user.bookmarks.find_or_create_by(problem_id: params[:problem_id])
     # M:To go back to the page right before, if not, back to root_path
-    redirect_back fallback_location: root_path, success: t('defaults.message.bookmark')
+    #redirect_back fallback_location: root_path, success: t('defaults.message.bookmark')
+    # M:To make it asynchronous communication
+    redirect_to request.referer
   end 
   
   def destroy
     # M:&=safe navigation operator (ref botchi enzanshi in Jp)
     current_user.bookmarks.find_by(problem_id: params[:problem_id])&.destroy!
-    redirect_back fallback_location: root_path, success: t('defaults.message.not_bookmark')
+    #redirect_back fallback_location: root_path, success: t('defaults.message.not_bookmark')
+    # M:To make it asynchronous communication
+    redirect_to request.referer
   end 
 end
